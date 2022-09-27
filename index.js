677292b1ff9c9b03f71feca9356074f207ddf06f
ahmed rashid <ahmeddmrashid@gmail.com>
2022-09-27 21:52:29 +0300

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+// document.querySelector('DOMContentLoaded', () => {
+//     document.querySelector("form").addEventListener('submit' , (e) => {
+//     e.preventdefault()
+//     handleToDo(e.target.new_todo.value)
+// })
+
+// })
+
+
+// function handleToDo(todo){
+//     let p = document.createElement('p')
+//     let btn = document.createElement('button')
+//     btn.textContent = 'x'
+//     p.textContent = `${todo}`
+//     p.appendChild(btn)
+//     console.log(p)
+//     document.querySelector('main-content').appendChild(p)
+
+
+
+
+// }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let taskinput = document.querySelector('#new_task_description')
+document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  handleToDo(e.target.new_task_description.value)
+  document.querySelector('#create-task-form').reset()
+})
 });
+
+function handleToDo(todo){
+  console.log(todo)
+let li = document.createElement('li')
+let btn = document.createElement('button')
+btn.textContent = 'x'
+li.textContent = `${todo}`
+btn.addEventListener('click', handleDelete)
+li.appendChild(btn)
+console.log(li)
+document.querySelector('#tasks').appendChild(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
